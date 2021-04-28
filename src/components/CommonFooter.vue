7fb6afd06f18317bbc6e8dd0ec596c12ed988819
yuka1007 <yuka1007@hotmail.co.jp>
2021-04-28 23:32:59 +0900

    first-commit

---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer id="footer">
+    <small>Copyright 2019 estra inc. All Rights Reserved.</small>
+  </footer>
+</template>
+
+<style scoped>
+#footer {
+  height: 80px;
+  text-align: center;
+  line-height: 80px;
+}
+</style>
